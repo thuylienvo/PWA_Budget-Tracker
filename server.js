0a090f91139dac66f6079077829dbade457c02e2
Mimi Vo <mimzy414@gmail.com>
2021-08-15 19:35:14 -0500

    deployed successfully to heroku FINALLY

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://bitesofmylife:Rymi505$MML?!475^2H2bs@cluster0.l7qza.mongodb.net/PWABudgetTracker?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
